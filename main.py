fc2657ad14a16b323709cb2af4d45e9bdc70dc5d
Natalia Badanina <>
2019-09-03 12:00:37 +0300

    added class and functions

--- main.py
+++ main.py
@@ -1,40 +1,55 @@
 import re
 import random
-
-file = open('inputText', 'r') #открываем файл с текстом для чтения
-data = file.read().lower() #читаем строчки из файла и преобразуем к нижнему регистру
-file.close() #закрываем файл
+import pickle
 
 
-data = re.sub(r'[.,!?:;–]', '', data) #заменяем всю пунктуацию на пустой символ
-data = re.sub(r'\n', ' ', data) #заменяем переносы строки на пробелы
-data = re.sub(r' +', ' ', data).split(' ') #заменяем все множественные пробелы на одинарные и разбиваем строку по пробелам
+class WordGenerator:
 
-dic = {}
+    @staticmethod
+    def dicGenerate(fileName):
 
-for i in range(len(data)-1):
-    if data[i] not in dic:
-        dic.update({data[i]: [data[i + 1]]})
-    else:
-        dic.update({data[i]: dic.get(data[i]) + [data[i + 1]] })
+        file = open(fileName, 'r', encoding='utf-8') #открываем файл с текстом для чтения
+        data = file.read().lower() #читаем строчки из файла и преобразуем к нижнему регистру
+        file.close() #закрываем файл
 
-for key in dic:
-    for i in range(len(dic.get(key))):
-        dic.update({key: dic.get(key) + [dic.get(key).count(dic.get(key)[i])] })
-        #print(dic.get(key).count(dic.get(key)[i]))
-firstWord = random.choice(list(dic.keys()))
-strlen = 5
-answer = ''
+        data = re.sub(r'[.,!?:;–()«»]', '', data) #заменяем всю пунктуацию на пустой символ
+        data = re.sub(r'\n', ' ', data) #заменяем переносы строки на пробелы
+        data = re.sub(r' +', ' ', data).split(' ') #заменяем все множественные пробелы на одинарные и разбиваем строку по пробелам
 
-print(firstWord)
+        dic = {}
 
-for i in range(strlen):
-    answer += firstWord + ' '
+        for i in range(len(data)-1):
+            if data[i] not in dic:
+                dic.update({data[i]: [data[i + 1]]})
+            else:
+                dic.update({data[i]: dic.get(data[i]) + [data[i + 1]]})
 
-    maxNum = max(dic.get(firstWord)[int(len(dic.get(firstWord))/2):])
-    firstWordIndex = dic.get(firstWord).index(maxNum)-len(dic.get(firstWord))/2
-    firstWord = dic.get(firstWord)[int(firstWordIndex)]
+        for key in dic:
+            for i in range(len(dic.get(key))):
+                dic.update({key: dic.get(key) + [dic.get(key).count(dic.get(key)[i])]})
 
+        # проблема с кодировкой
+        # with open('outputDictionary', 'wb') as fileWrite:
+        #     pickle.dump(dic, fileWrite)
+        return dic
 
-print(dic)
-print(answer)
+    @staticmethod
+    def answerGenerate(strlen, **dic):
+
+        firstWord = random.choice(list(dic.keys()))
+        answer = ''
+
+        for i in range(strlen):
+            answer += firstWord + ' '
+
+            maxNum = max(dic.get(firstWord)[int(len(dic.get(firstWord))/2):])
+            firstWordIndex = dic.get(firstWord).index(maxNum)-len(dic.get(firstWord))/2
+            firstWord = dic.get(firstWord)[int(firstWordIndex)]
+
+        return answer
+
+wordGen = WordGenerator()
+dictionary = wordGen.dicGenerate('inputText')
+string = wordGen.answerGenerate(10, **dictionary)
+
+print(string)
